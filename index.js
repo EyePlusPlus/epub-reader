aa8c556afc5ffa1443f5449d17ca897a91520e9e
Mansi Shah <mansi@oml.in>
2018-01-07 00:03:36 +0530

    jsdom works

---
+++ index.js
@@ -0,0 +1,17 @@
+const { JSDOM } = require('jsdom');
+
+const html = `<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+</head>
+<body>
+
+</body>
+</html>`;
+
+const dom = new JSDOM(html);
+console.log(typeof dom);
