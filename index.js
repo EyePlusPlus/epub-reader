2a203e2b3cc23c105be9c142605a682bf394c0a5
Mansi Shah <mansi@oml.in>
2018-01-08 02:58:37 +0530

    wip

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 const { JSDOM } = require('jsdom');
 const blessed = require('blessed');
+const EPub = require('epub');
 
 const screen = blessed.screen({
 	smartCSR: true
 });
-screen.title = 'my window title';
-const html = `<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Document</title>
-</head>
-<body>
 
-</body>
-</html>`;
+const epub = new EPub('./riaklil-en.zip', __dirname, __dirname);
+epub.on("end", function(){
+	// epub is now usable
+	screen.title = epub.metadata.title;
 
-const dom = new JSDOM(html);
-console.log(typeof dom);
-screen.render();
+	epub.flow.forEach(function(chapter){
+		console.log(chapter.id);
+	});
+
+	epub.getChapter("html35", function(err, text){
+		console.log("here");
+		// const dom = new JSDOM(html);
+		// console.log(typeof dom);
+		// screen.render();
+
+	});
+});
+epub.parse();
