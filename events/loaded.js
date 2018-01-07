e5221dbfeeba3b23cac2ef294b512054dcbff0cf
Mansi Shah <mansi@oml.in>
2018-01-08 04:50:34 +0530

    Book loads and renders

---
+++ events/loaded.js
@@ -0,0 +1,75 @@
+const blessed = require('blessed');
+const { JSDOM } = require('jsdom');
+const screen = blessed.screen({
+	smartCSR: true
+});
+
+const getContent = (value) => {
+	const contentBox = blessed.box({
+		content: `This is ${value}`,
+		tags: true,
+		border: {
+			type: 'line'
+		},
+		scrollable: true,
+		alwaysScroll: true,
+		style: {
+			fg: 'white',
+			border: {
+				fg: '#f0f0f0'
+			},
+			hover: {
+				bg: 'green'
+			}
+		}
+	});
+	return contentBox;
+};
+
+
+module.exports = function onLoaded ({epub}) {
+	// epub is now usable
+	screen.title = epub.metadata.title;
+	const tocList = [];
+
+	epub.flow.forEach(function(chapter){
+		tocList.push(chapter.id);
+	});
+
+	const list = blessed.list({
+		keys: true,
+		border: 'line',
+		items: tocList,
+		style: {
+			selected: {
+				bg: 'green'
+			}
+		}
+	});
+
+	screen.append(list);
+
+	list.on('select', (data, idx) => {
+		epub.getChapter(data.content, function(err, text){
+			// const dom = new JSDOM(html);
+			const newContent = getContent(text);
+
+			screen.remove(list);
+			screen.append(newContent);
+			screen.render();
+
+			newContent.key('left', () => {
+				screen.remove(newContent);
+				screen.append(list);
+				screen.render();
+			});
+		});
+	});
+
+	screen.key(['escape', 'q', 'C-c'], function(ch, key) {
+		return process.exit(0);
+	});
+
+	list.focus();
+	screen.render();
+};
--- index.js
+++ index.js
@@ -1,26 +1,8 @@
-const { JSDOM } = require('jsdom');
-const blessed = require('blessed');
 const EPub = require('epub');
-
-const screen = blessed.screen({
-	smartCSR: true
-});
+const onLoaded = require('./events/loaded.js');
 
 const epub = new EPub('./riaklil-en.zip', __dirname, __dirname);
-epub.on("end", function(){
-	// epub is now usable
-	screen.title = epub.metadata.title;
-
-	epub.flow.forEach(function(chapter){
-		console.log(chapter.id);
-	});
-
-	epub.getChapter("html35", function(err, text){
-		console.log("here");
-		// const dom = new JSDOM(html);
-		// console.log(typeof dom);
-		// screen.render();
-
-	});
+epub.on("end", () => {
+	onLoaded({epub});
 });
 epub.parse();
