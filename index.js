0e94a73ff99bf26c82cd02428dcde8813f7d81c4
Mansi Shah <mansi@oml.in>
2018-01-10 13:52:26 +0530

    wip

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const _ = require('lodash');
 const parser = require('epub-parser');
 const onLoaded = require('./events/loaded.js');
 
-const epub = parser.open('./riaklil-en.zip', (err, data) => {
+if (_.isUndefined(process.argv[2])) {
+	console.log("Pass the epub file name as the first argument");
+	process.exit(1);
+}
+
+const epub = parser.open(process.argv[2], (err, data) => {
 	if (err) {
 		console.log(err);
 		process.exit(1);
